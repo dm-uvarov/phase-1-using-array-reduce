3890f3c11444759d8b215d5f569a03d8fa7560a4
Jimmy <dm.a.uvarov@gmail.com>
2022-01-14 14:47:56 -0500

    commit tree 80578e0bee2d7e736d8108b8bd0fad8698f89130

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+//const reducer = (prevVal,curVal) => prevVal + curVal;
+
+// const totalBatteries = batteryBatches.reduce(reducer)
+
+const totalBatteries = batteryBatches.reduce((pv,cv)=>pv+cv )
